16e670be404fea4ee57b6c9d2fa1373883264585
Alisson Max <alissonchan@hotmail.com>
2023-10-22 11:57:40 -0300

    desafio feito

---
+++ iPhone.java
@@ -0,0 +1,34 @@
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+  //Reprodutor Musical
+  public void tocar() { 
+    // código para tocar música  
+  }
+  public void pausar() {
+    // código para pausar música  
+  }
+  public void selecionarMusica(String musica) {
+    // código para selecionar música  
+  }
+
+  //Aparelho Telefônico
+  public void ligar(String numero) { 
+    // código para ligar  
+  }
+  public void atender() { 
+    // código para atender  
+  }
+  public void iniciarCorreioVoz() { 
+    // código para iniciar correio de voz 
+  }
+
+  // Navegador Internet
+  public void exibirPagina(String url) { 
+    // código para exibir página  
+  }
+  public void adicionarNovaAba(String url) { 
+    // código para adicionar nova aba 
+  }
+  public void atualizarPagina() { 
+    // código para atualizar página  
+  }
+}
